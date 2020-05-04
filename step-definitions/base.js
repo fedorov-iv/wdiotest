3fcb028f791629e788bf8513abdcbfdb64ffae35
FEDOROV Igor <Igor.FEDOROV@raiffeisen.ru>
2020-05-04 11:24:39 +0300

    initial commit

---
+++ step-definitions/base.js
@@ -0,0 +1,16 @@
+const { Given } = require('cucumber');
+const { Then } = require('cucumber');
+const assert = require("assert");
+
+
+Given(
+    /^I get the main page$/, function(){
+        browser.url("/");
+    });
+
+Then(
+    /^Page contains title (.+)$/, function (expected){
+        let title = browser.getTitle();
+        assert.strictEqual(title, expected);
+
+    });
