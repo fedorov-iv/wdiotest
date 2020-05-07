8b37dd22b9abf3089aad4c5bcdb683fe9674b040
FEDOROV Igor <Igor.FEDOROV@raiffeisen.ru>
2020-05-07 10:33:46 +0300

    cucumber 4 step definitions

--- features/main.feature
+++ features/main.feature
@@ -1,4 +1,4 @@
 Feature: Main Page
   Scenario: Open Main Page
     Given I get the main page
-    Then Page contains title WebdriverIOsdd · Next-gen browser and mobile automation test framework for Node.js
+    Then Page contains title "WebdriverIOsdd · Next-gen browser and mobile automation test framework for Node.js"
--- step-definitions/base.js
+++ step-definitions/base.js
@@ -1,15 +1,15 @@
 const { Given } = require('cucumber');
 const { Then } = require('cucumber');
-const assert = require("assert");
+const  assert  = require("assert");
 
 
 Given(
-    /^I get the main page$/, function(){
+    "I get the main page", function(){
         browser.url("/");
     });
 
 Then(
-    /^Page contains title (.+)$/, function (expected){
+    "Page contains title {string}", function (expected){
         let title = browser.getTitle();
         assert.strictEqual(title, expected);
 
